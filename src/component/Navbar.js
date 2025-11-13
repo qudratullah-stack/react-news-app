94b4e94edd856b5e050d01c38762bdf4b0f35904
QudratUllah <qudratullahmasoom6688@gmail.com>
2025-11-12 21:27:07 -0800

    i start the project and make three component

---
+++ src/component/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar(props) {
+  return (
+    <div>
+      <h1>{props.navbar_h1}</h1>
+    </div>
+  )
+}
---
+++ src/component/New_Item.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function New_Item() {
+  return (
+    <>
+    <div className="item">
+        <img src="" alt="no image" />
+        <h2>Title</h2>
+        <p>Discription</p>
+        <button>Read More</button>
+    </div>
+    </>
+  )
+}
